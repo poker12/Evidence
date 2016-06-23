c76027c5fef141caf757f77e33505c9bbd6e8812
Tomáš <Tom@MSI-Tommy>
2016-06-23 23:11:14 +0200

    manageProductCategory manageVatRate product create

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/gui/main/Main.fxml"));
+			//Parent root = FXMLLoader.load(getClass().getResource("/gui/main/Main.fxml"));
+			//Parent root = FXMLLoader.load(getClass().getResource("/gui/product/manageProductCategory/manageProductCategory.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/gui/vatRate/manageVatRate/manageVatRate.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
